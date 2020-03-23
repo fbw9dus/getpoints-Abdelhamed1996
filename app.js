8939989915877bb35949f14dbd1fe5602ddc5f7b
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2020-03-23 11:52:55 +0100

    test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,8 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter   = require('./routes/api')
 
 var app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,4 +22,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var user = [{user:"fake", password:"fake"}]
+  res.status(200).json(user)
 });
-
-
 
 module.exports = router;
